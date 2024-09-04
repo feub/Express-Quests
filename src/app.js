6ad127b71f25d52a7129fd54a35bb73468d87de9
Fabien <fabien@feub.net>
2024-09-04 15:29:07 +0200

    Movies and Users are now updatable

--- src/app.js
+++ src/app.js
@@ -7,11 +7,13 @@ const movieControllers = require("./controllers/movieControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.post("/api/movies", movieControllers.postMovie);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.put("/api/movies/:id", movieControllers.updateMovie);
 
 const userControllers = require("./controllers/userControllers");
 
 app.get("/api/users", userControllers.getUsers);
 app.post("/api/users", userControllers.postUser);
 app.get("/api/users/:id", userControllers.getUserById);
+app.put("/api/users/:id", userControllers.updateUser);
 
 module.exports = app;
